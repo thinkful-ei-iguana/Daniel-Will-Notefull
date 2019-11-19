e87c5891b7b874fbbca129990c9076f5858dfc1c
Sokos6 <will.sokolowski@gmail.com>
2019-11-19 14:06:08 -0600

    dummyconnect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
-import AppHeader from './AppHeader';
-import FolderList from './FolderList';
-import NoteList from './NoteList';
-import NoteNavigation from './NoteNavigation';
-import NotePage from './NotePage';
-import {getNotes, findNote, findFolder} from './notehelpers';
-import dummyStore from './dummyStore';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import AppHeader from "./AppHeader";
+import FolderList from "./FolderList";
+import NoteList from "./NoteList";
+import NoteNavigation from "./NoteNavigation";
+import NotePage from "./NotePage";
+import { getNotes, findNote, findFolder } from "./notehelpers";
+import dummyStore from "./dummyStore";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -15,72 +15,66 @@ class App extends Component {
     folders: []
   };
 
+  componentDidMount() {
+    setTimeout(() => this.setState(dummyStore), 600);
+  }
+
   renderListRoutes() {
-    const {notes, folders} = this.state;
+    const { notes, folders } = this.state;
     return (
       <>
-        {['/', '/folder/:folderId'].map(path => (
-          <Route 
-            exact  
+        {["/", "/folder/:folderId"].map(path => (
+          <Route
+            exact
             key={path}
             render={routeProps => (
-              <FolderList 
-                folders={folders}
-                notes={notes}
-                {...routeProps}
-              />
+              <FolderList folders={folders} notes={notes} {...routeProps} />
             )}
           />
         ))}
 
-        <Route 
-          path='/note/:noteId'
+        <Route
+          path="/note/:noteId"
           render={routeProps => {
-            const {noteId} = routeProps.match.params;
+            const { noteId } = routeProps.match.params;
             const note = findNote(notes, noteId) || {};
             const folder = findFolder(folders, note.folderId);
             return <FolderList {...routeProps} folder={folder} />;
           }}
         />
-        <Route path='add-folder' component={FolderList} />
-        <Route path='add-note' component={FolderList} />
+        <Route path="add-folder" component={FolderList} />
+        <Route path="add-note" component={FolderList} />
       </>
-
     );
   }
 
   renderMainRoutes() {
-    const {notes, folders} = this.state;
+    const { notes, folders } = this.state;
     return (
       <>
-        {['/', '/folder/:folderId'].map(path => (
-          <Route 
-            exact  
+        {["/", "/folder/:folderId"].map(path => (
+          <Route
+            exact
             key={path}
             render={routeProps => {
-              const {folderId} = routeProps.match.params;
+              const { folderId } = routeProps.match.params;
               const notesForFolder = getNotes(notes, folderId);
-              return (
-                <NoteList 
-                  {...routeProps}
-                  notes={notesForFolder}
-                />
-              );
+              return <NoteList {...routeProps} notes={notesForFolder} />;
             }}
           />
         ))}
 
-        <Route 
-          path='/note/:noteId'
+        <Route
+          path="/note/:noteId"
           render={routeProps => {
-            const {noteId} = routeProps.match.params;
+            const { noteId } = routeProps.match.params;
             const note = findNote(notes, noteId);
             const folder = findFolder(folders, note.folderId);
             return <NotePage {...routeProps} note={note} />;
           }}
         />
-        <Route path='add-folder' component={FolderList} />
-        <Route path='add-note' component={FolderList} />
+        <Route path="add-folder" component={FolderList} />
+        <Route path="add-note" component={FolderList} />
       </>
     );
   }
@@ -88,25 +82,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav>
-          {this.renderListRoutes()}
-        </nav>
+        <nav>{this.renderListRoutes()}</nav>
         {/* <AppHeader /> */}
         <header className="app-header">
           <h1>
-            <Link to='/'>
-              Noteful
-            </Link>
+            <Link to="/">Noteful</Link>
           </h1>
         </header>
-        <main>
-          {this.renderMainRoutes()}
-        </main>
+        <main>{this.renderMainRoutes()}</main>
       </div>
     );
   }
 }
-
-
 
 export default App;
