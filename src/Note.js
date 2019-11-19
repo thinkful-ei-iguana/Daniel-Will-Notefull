e759fb14768e931b89b7c983f293467ca7eb91e7
Sokos6 <will.sokolowski@gmail.com>
2019-11-19 14:27:06 -0600

    added moment

--- src/Note.js
+++ src/Note.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { format } from 'path';
+import React from "react";
+import { Link } from "react-router-dom";
+// import format from "date-fns/format";
+import * as moment from "moment";
 
 export default function Note(props) {
   return (
     <div className="note-item">
       <h2 className="note-title">
-        <Link to={`/note/${props.id}`} >
-          {props.name}
-        </Link>
+        <Link to={`/note/${props.id}`}>{props.name}</Link>
       </h2>
-      <button className="delete-note-btn" type='button'>
-        {''}
+      <button className="delete-note-btn" type="button">
+        {""}
         Remove
       </button>
       <div className="note-date-mod">
         modified
-        {''}
-        <span className='date'>
-          {format(props.modified, 'Do MMM YYYY')}
+        {""}
+        <span className="date">
+          {moment().format(props.modified, "Do MMM YYYY")}
         </span>
       </div>
     </div>
