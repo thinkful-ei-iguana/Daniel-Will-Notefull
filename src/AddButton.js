125dfea38281e41f8a0fa0c8131b3dffe4a5ae7b
Daniel Kent <Kentpercussion@gmail.com>
2019-11-19 10:28:55 -0700

    added Button Component

--- src/AddButton.js
+++ src/AddButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 
+export default function AddButton(props) {
+  const {tag, className, children, ...otherProps} = props;
+
+  return (
+    props.tag,
+    {
+      className:['add-button', props.className].join(' '),
+      ...otherProps
+
+    },
+    props.children
+  );
+}
+AddButton.defaultProps= {
+  tag: 'a'
+}
