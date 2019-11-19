c83fbb042a5700f0a355ab6c0124b29caa126e90
Daniel Kent <Kentpercussion@gmail.com>
2019-11-19 10:19:33 -0700

    added Note List Component

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import Note from './Note'
+import Link from 'react-router-dom';
+import Note from './Note';
+import AddButton from './AddButton';
 
 export default function NoteList(props) {
+  
+  
   const notes = props.notes.map(note => {
+
     return (
       <Note note={note} />
     );
   });
 
   return (
-    <div className="note-list">
-      {notes}
-    </div>
+    <section className='note-list'>
+      <ul>
+        {props.notes.map(note => 
+          <li key={note.id}>
+            <Note 
+              id={note.id}
+              name={note.name}
+              modified={note.modified}
+            />
+          </li>
+        )}
+      </ul>
+
+      <div>
+        <AddButton 
+          tag={Link}
+          to='/add-note'
+          type='button'
+          className='add-note-btn'
+        >
+          note
+        </AddButton>
+      </div>
+    </section>
   );
 }
